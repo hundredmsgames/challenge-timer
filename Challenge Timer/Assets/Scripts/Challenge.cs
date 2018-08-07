4b7d7fcc598df207490b2fc09fe186f7c3af3318
SOFTSAM <sml.ozclk@gmail.com>
2018-08-07 17:40:02 +0300

    small additions

---
+++ Challenge Timer/Assets/Scripts/Challenge.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Challenge  {
+
+    string name;
+    ChallengeLevel challengeLevel;
+    int timeInterval;
+    float error;
+    public delegate void ChallengeHandler(object sender, string name);
+    public event ChallengeHandler ChallengeCompleted;
+
+}
---
+++ Challenge Timer/Assets/Scripts/Enums/enums.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    enum ChallengeLevel : byte
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
