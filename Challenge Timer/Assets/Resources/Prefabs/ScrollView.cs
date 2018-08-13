fa5798e4a82947480270aba2cc2bc1681574a97d
Melih <mlhtncc@gmail.com>
2018-08-13 11:58:42 +0300

    Small Fixes

--- Challenge Timer/Assets/Resources/Prefabs/ScrollView.cs
+++ Challenge Timer/Assets/Resources/Prefabs/ScrollView.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScrollView : MonoBehaviour {
-
-     RectTransform scrollViewPanel;
+public class ScrollView : MonoBehaviour
+{
+    RectTransform scrollViewPanel;
     bool showListPanel = false;
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
         scrollViewPanel = this.GetComponent<RectTransform>();
 	}
+
     float bottom = 0;
-	// Update is called once per frame
+	
+    // Update is called once per frame
 	void Update () {
         if (showListPanel)
         {
@@ -22,18 +25,27 @@ public class ScrollView : MonoBehaviour {
         {
             HideListPanel();
         }
-	}
+    }
+
     public void ShowListPanelbool()
     {
         
         showListPanel = !showListPanel;
     }
+
     public void ShowListPanel()
     {
-        scrollViewPanel.sizeDelta=new Vector2(scrollViewPanel.sizeDelta.x,Mathf.Lerp(scrollViewPanel.sizeDelta.y, 500, Mathf.SmoothStep(0.0f, 1.0f, Time.deltaTime*10)));
+        scrollViewPanel.sizeDelta = new Vector2(
+            scrollViewPanel.sizeDelta.x,
+            Mathf.Lerp(scrollViewPanel.sizeDelta.y, 500, Time.deltaTime * 5f)
+        );
     }
+
     public void HideListPanel()
     {
-        scrollViewPanel.sizeDelta=new Vector2(scrollViewPanel.sizeDelta.x,Mathf.Lerp(scrollViewPanel.sizeDelta.y, 90, Mathf.SmoothStep(0.0f, 1.0f, Time.deltaTime*10)));
+        scrollViewPanel.sizeDelta = new Vector2(
+            scrollViewPanel.sizeDelta.x,
+            Mathf.Lerp(scrollViewPanel.sizeDelta.y, 90, Time.deltaTime * 5f)
+        );
     }
 }
